d1d1a89918e57977f71a8f6193717d1423456382
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2019-12-18 11:30:41 +0200

    now

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<form>
+    <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Janis Ozols">
+    <label for="userName">Username</label>
+    <input type="password" name="password" id="">
+    <label for="email"><i class="fa fa-envelope"></i> Email</label>
+    <input type="text" id="email" name="email" placeholder="Janis@example.com">
+    <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+    <input type="text" id="adr" name="address" placeholder="Rozu iela 14A">
+    <label for="city"><i class="fa fa-institution"></i> City</label>
+    <input type="text" id="city" name="city" placeholder="Riga">
+</form>
