e0c71a812751e517e8cb7f9260777a5f570ab16d
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2020-01-09 11:24:05 +0200

    script

--- scripts/script.js
+++ scripts/script.js
@@ -37,13 +37,33 @@ function myFunction() {
     }
 }
 
+/**
+ * Navigation menu bar
+ */
+
+$(window).resize(function() {
+    if (($(window).width() >= "980") && (!$(".menu-bar").is(":visible"))) {
+        console.log($(window).width());
+        $(".menu-bar").show();
+    } else if (($(window).width() < "980") && ($(".menu-bar").is(":visible"))) {
+        $(".menu-bar").hide();
+    }
+});
 
 
 $(document).ready(function() {
 
-/**
- * manages the display of images in modal
- */
+    /**
+     * navigation menu bar
+     */
+
+    $('#burger').on('click',function() {
+        $(".menu-bar").slideToggle('300');
+    });
+
+    /**
+     * manages the display of images in modal
+     */
 
     if (typeof imgSeq !== 'undefined'){
         $('.imgInput').click(function (e) {
@@ -60,6 +80,17 @@ $(document).ready(function() {
             let img = $('#img'+imgSeq);
             loadImg(img);
         });
+    }
+
+    /**
+     * modal for images
+     * @param img
+     */
+
+    function loadImg(img) {
+        let srcLg = img.data('src');
+        $('#imgInModal').attr('src', srcLg);
+        $('#exampleModalCenter').modal('show');
     }
 
     /**
@@ -100,70 +131,25 @@ $(document).ready(function() {
         window.location.replace("user.php");
     })
 
+        /**
+         * registration buttons
+         */
+
+        ($('.regButton'));
+    $('.regButton').click(function (e) {
+        e.preventDefault();
+        $('.regForm').slideToggle('300');
+    });
+
+    $('.dropBtn').click(function (e) {
+        e.preventDefault();
+        $(this).siblings('.dropDown').slideToggle('300');
+    });
+
 });
 
-/**
- * navigation menu bar
- */
-
-$('#burger').on('click',function() {
-    $(".menu-bar").slideToggle('300');
-});
-
-$(window).resize(function() {
-    if (($(window).width() >= "980") && (!$(".menu-bar").is(":visible"))) {
-        console.log($(window).width());
-        $(".menu-bar").show();
-    } else if (($(window).width() < "980") && ($(".menu-bar").is(":visible"))) {
-        $(".menu-bar").hide();
-    }
-});
-
-/**
- * registration buttons
- */
-
-($('.regButton'));
-$('.regButton').click(function (e) {
-    e.preventDefault();
-    $('.regForm').slideToggle('300');
-});
-
-$('.dropBtn').click(function (e) {
-    e.preventDefault();
-    $(this).siblings('.dropDown').slideToggle('300');
-});
-
-/* modal increment by one for img */
-/**
-
-$('.col').click(function() {
-    $('.modal-body').empty();
-    let title = $(this).parent('a').attr("title");
-    $('.modal-title').html(title);
-    $($(this).parents('div').html()).appendTo('.modal-body');
-    $('#imgInModal').modal({show:true});
-});**/
-
-/**
- * modal for images
- * @param img
- */
-
-function loadImg(img) {
-    let srcLg = img.data('src');
-    $('#imgInModal').attr('src', srcLg);
-    $('#exampleModalCenter').modal('show');
-}
-
-/* modal increment end */
 
 
-/*
-    Get data from the form upon submission (username and message)
-    Make ajax call to sendMessage.php
-    Read answer from the call and inform user (success vs error)
- */
 import './cart';
 import './chat';
 import './cookies';
